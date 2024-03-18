05a56240703ee7ba673d16dae05aa78eeea04c98
Musanna Al Akil <musannaal@gmail.com>
2024-03-18 16:59:42 +0600

    hello

--- app/components/ProductCard.tsx
+++ app/components/ProductCard.tsx
@@ -1,10 +1,10 @@
-'use client';
+
 import React from 'react'
 
 function ProductCard() {
   return (
     <div>
-        <button onClick={()=> console.log('click')}>Add to cart</button>
+        <button >Add to cart</button>
     </div>
   )
 }
